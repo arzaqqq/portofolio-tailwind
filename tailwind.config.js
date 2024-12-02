5c59dd51ec6c1d3303830555a351cbb829b63deb
arzaqqq <mmiftahularzaq01@gmail.com>
2024-12-02 15:41:24 +0700

    add about

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,10 @@ module.exports = {
         'primary': '#14b8a6',
         'dark': 'rgb(15 23 42)',
         'abu': 'rgb(100 116 139)',
+        'lingkaran': 'rgb(203 213 225)',
       },
       screens: {
-        '2xl' : '1320px',
+        'xl' : '1320px',
       },
     },
   },
